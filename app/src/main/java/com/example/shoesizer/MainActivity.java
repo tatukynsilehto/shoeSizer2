dd09e67bae8dd5b74120b23559cca28f41c123dd
tatukynsilehto <tatu.kynsilehto@hotmail.com>
2021-05-11 05:09:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/shoesizer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.shoesizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] arraySpinner = new String[]{
+                "Mens", "Womens"
+        };
+        Spinner s = (Spinner) findViewById(R.id.spinner01);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+    }
+}
