4f5a32c9127bcac569d98d8224d606902ca6a106
tatukynsilehto <tatu.kynsilehto@hotmail.com>
2021-05-20 10:08:57 +0300

    toiminnallisuutta

--- app/src/main/java/com/example/shoesizer/MainActivity.java
+++ app/src/main/java/com/example/shoesizer/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,86 +30,119 @@ public class MainActivity extends AppCompatActivity {
                 "Nike", "Adidas"
         };
 
-        String[] menUs = new String[]{
-                "7", "8", "8.5", "9", "9.5", "10", "11", "12", "13"
+        String[] nikemenUs = new String[]{
+                "6", "6.5", "7", "7,5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "13", "14", "15" //16
         };
 
-        String[] menEu = new String[]{
-                "40", "41", "42", "42.5", "43", "44", "45", "46", "47"
+        String[] nikemenEu = new String[]{
+                "38.5", "39", "40", "40.5", "41", "42", "42.5", "43", "44", "44.5", "45", "45.5", "46", "47.5", "48.5", "49.5" //16
         };
 
-        String[] menUk = new String[]{
-                "6", "7", "7.5", "8", "8.5", "9", "10", "11", "12"
+        String[] nikemenUk = new String[]{
+                "5.5", "6", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "12", "13", "14" //16
         };
 
-        String[] menCm = new String[]{
-                "25", "26", "26.5", "27", "27.5", "28", "29", "30", "31"
+        String[] nikemenCm = new String[]{
+                "24", "24.5", "25", "25.5", "26", "26.5", "27", "27.5", "28", "28.5", "29", "29.5", "30", "31", "32", "33" //16
         };
 
-        String[] womenUs = new String[]{
-                "5", "5.5", "6", "6.5", "7", "8", "8.5", "9.5", "10"
+        String[] nikewomenUs = new String[]{
+                "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12" //16
         };
 
-        String[] womenUk = new String[]{
-                "2.5", "3", "3.5", "4", "4.5", "5.5", "6", "7", "7.5"
+        String[] nikewomenUk = new String[]{
+                "2.5", "3", "3.5", "4", "4.5", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5" //16
         };
 
-        String[] womenEu = new String[]{
-                "35.5", "36", "36.5", "37.5", "38", "39", "40", "41", "42"
+        String[] nikewomenEu = new String[]{
+                "35.5", "36", "36.5", "37.5", "38", "38.5", "39", "40", "40.5", "41", "42", "42.5", "43", "44", "44.5" //16
         };
 
-        String[] womenCm = new String[]{
-                "22", "22.5", "23", "23.5", "24", "25", "25.5", "26.5", "27"
+        String[] nikewomenCm = new String[]{
+                "22", "22.5", "23", "23.5", "24", "24.5", "25", "25.5", "26", "26.5", "27", "27.5", "28", "28.5", "29" //16
         };
 
+        String[] adidasmenUs = new String[]{
+                "6", "6.5", "7", "7,5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "13", "14", "15" //16
+        };
+
+        String[] adidasmenUk = new String[]{
+                "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12.5", "13.5", "14.5" //16
+        };
+
+        String[] adidasmenEu = new String[]{
+                "38 2/3", "39 1/3", "40", "40 2/3", "41 1/3", "42", "42 2/3", "43 1/3", "44", "44 2/3", "45 1/3", "46", "46 1/3", "48", "49 1/3", "50 2/3" //16
+        };
+
+        String[] adidasmenCm = new String[]{
+                "24", "24.5", "25", "25.5", "26", "26.5", "27", "27.5", "28", "28.5", "29", "29.5", "30", "31", "32", "33" //16
+        };
+
+
+        TextView result = (TextView) findViewById(R.id.textView7);
+
+        //gender
         Spinner s = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, arraySpinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);
 
+        //us eu etc...
         Spinner s1 = (Spinner) findViewById(R.id.spinner01);
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arraySpinner1);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s1.setAdapter(adapter1);
 
+        //brand
         Spinner s2 = (Spinner) findViewById(R.id.spinner2);
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arraySpinner2);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s2.setAdapter(adapter2);
 
+        //size
         Spinner s3 = (Spinner) findViewById(R.id.spinner3);
 
+        //brand
         Spinner s4 = (Spinner) findViewById(R.id.spinner4);
         s4.setAdapter(adapter2);
 
-        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, menUs);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nikemenUs);
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> adapter4 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, menEu);
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nikemenEu);
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> adapter5 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, menUk);
+        ArrayAdapter<String> adapter5 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nikemenUk);
         adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> adapter6 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, menCm);
+        ArrayAdapter<String> adapter6 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nikemenCm);
         adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> adapter7 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, womenUs);
+        ArrayAdapter<String> adapter7 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nikewomenUs);
         adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> adapter8 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, womenUk);
+        ArrayAdapter<String> adapter8 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nikewomenUk);
         adapter8.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> adapter9 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, womenEu);
+        ArrayAdapter<String> adapter9 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nikewomenEu);
         adapter9.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        ArrayAdapter<String> adapter10 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, womenCm);
+        ArrayAdapter<String> adapter10 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nikewomenCm);
         adapter10.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    s1.setAdapter(adapter1);
+                    s2.setAdapter(adapter2);
+                    s3.setAdapter(adapter3);
+
+
+                }
                 if(position==1){
+                    s1.setAdapter(adapter1);
+                    s2.setAdapter(adapter2);
                     s3.setAdapter(adapter7);
                     women = 1;
                 }
@@ -147,6 +181,7 @@ public class MainActivity extends AppCompatActivity {
                 if(position==3 && women==1){
                     s3.setAdapter(adapter10);
                 }
+
             }
 
             @Override
